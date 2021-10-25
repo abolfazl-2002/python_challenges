48b08d16a470a0ae7ecf420b8ea342d8e26d8bdd
Abolfazl M <abolfazl.mk.2002@gmail.com>
2021-10-25 20:49:34 +0330

    add surname

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-# print user name
+# print user full name
+# add surname
 
-name = input("what is your name:\n")
+firstname = input("what is ur firstname:\n") #firstname
+surname=input("what is ur surname:\n") #surname
 
-print("The user name is",name,"\nHello",name)
+print("The user name is",firstname,surname , "\nHello",firstname,surname)
